0c90efddc2bbf58a1f0af11c772989961971ac65
kamalnathvg <kamalnath7171@gmail.com>
2024-07-02 17:00:39 -0400

    added 3 pages

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomePage from './screens/HomePage';
+import RandomImageGenerator from './screens/Page1';
+import Page3 from './screens/Page3';
+import Page1 from './screens/Page1';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='HomePage'>
+          <Stack.Screen name='HomePage' component={HomePage}/>
+          <Stack.Screen name='Page1' component={Page1}/>
+          <Stack.Screen name='Page3' component={Page3}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+      );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
